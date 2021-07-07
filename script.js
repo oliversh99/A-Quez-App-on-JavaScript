fe7cc9989e042615148b5f6dd08ba8d7b7b08351
oliversh99 <oliv.code999@gmail.com>
2021-07-06 23:17:30 -0700

    push project to github first time

---
+++ script.js
@@ -0,0 +1,21 @@
+var startButton = document.getElementById('start-btn');
+var questionContainerElement = document.getElementById('question-container');
+
+
+ startButton.addEventListener('click', startGame);
+
+
+function startGame(){
+    console.log('Started');
+    startButton.classList.add('hide');
+
+}
+
+function setNextQuestion(){
+
+
+}
+
+function selectAnswer(){
+
+}
